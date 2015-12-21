667633160df71679249ae40e047dfe1d6c779193
uzilk <uzi.lk@travelersbox.com>
2015-12-21 14:14:31 +0000

    Just test things

---
+++ MyProjects/DummyConsole/DummyConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan sp1 = TimeSpan.FromDays(10);
+            TimeSpan sp2 = TimeSpan.FromDays(40);
+            TimeSpan sp3 = TimeSpan.FromDays(100);
+            TimeSpan sp4 = TimeSpan.FromDays(500);
+
+
+            int test = sp1.Days;
+            test = sp2.Days;
+            test = sp3.Days;
+            test = sp4.Days;
+        }
+    }
+}
